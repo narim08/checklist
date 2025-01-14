459180f9e9cf4757882a2c98a01764d30b410235
Narim Park <155282079+narim08@users.noreply.github.com>
2025-01-14 13:10:44 +0900

    Update ChallengeController.java

--- src/main/java/rabo/checklist/controller/ChallengeController.java
+++ src/main/java/rabo/checklist/controller/ChallengeController.java
@@ -1,6 +1,7 @@
 package rabo.checklist.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
