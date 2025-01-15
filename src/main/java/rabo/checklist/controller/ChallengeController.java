987bc0574077915f2c1c0f1b9d31386a400ea63e
Narim Park <155282079+narim08@users.noreply.github.com>
2025-01-15 19:01:32 +0900

    Add files via upload

--- src/main/java/rabo/checklist/controller/ChallengeController.java
+++ src/main/java/rabo/checklist/controller/ChallengeController.java
@@ -33,6 +33,7 @@ public class ChallengeController {
     public String mainPage(Model model) {
         List<ChallengeChecklist> challenges = challengeService.getAllChallenges();
         int completedDays = challengeService.getCompletedDays();
+
         model.addAttribute("challenges", challenges);
         model.addAttribute("completedDays", completedDays);
         return "main"; // main.html
